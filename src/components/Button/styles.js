71f07ec898f06708a05bd78b907a8dbccc645733
Ricardo-Gabriel <69832153+Ricardo-Gabriel@users.noreply.github.com>
2024-09-19 18:45:37 -0300

    Desafio-Calculadora

---
+++ src/components/Button/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    width: 60px;
+    border: 1px solid #818080;
+    background-color: #d2691e;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex:1;
+    
+    &:hover{
+        opacity: 0.6;
+    }
+
+
+`
